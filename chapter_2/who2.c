b44a9df7312782eb2dde66ae708dc5e9cbc01244
Chen Tuo <ch122633@126.com>
2017-12-29 12:52:31 +0800

    who2

---
+++ chapter_2/who2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define SHOWHOST
+
+void show_timer(long );
+void show_info(struct utmp *);
+
+int main()
+{
+	struct utmp current_record;		/* read info into here */
+	int utmpfd;
+	int reclen = sizeof(current_record);
+
+	if((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1)
+	{
+		perror(UTMP_FILE);
+		exit(1);
+	}
+
+	while(read(utmpfd, &current_record, reclen) == reclen)
+		show_info(&current_record);
+	close(utmpfd);
+	return 0;
+}
+
+void show_timer(long timeval)
+{
+	char *cp;
+	cp = ctime(&timeval);
+
+	printf("%12.12s", cp+4);
+}
+
+void show_info(struct utmp *utbufp)
+{
+	if(utbufp->ut_type != USER_PROCESS) 	/* users only */
+		return;
+	printf(" % -8.8s", utbufp->ut_name);
+	printf(" ");
+	printf(" % -8.8s", utbufp->ut_line);
+	printf(" ");
+	show_timer(utbufp->ut_time);
+	printf(" ");
+#ifdef SHOWHOST
+	printf("(%s)", utbufp->ut_host);
+#endif
+	printf("\n");
+}
